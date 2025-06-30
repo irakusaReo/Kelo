5fb523357d3aa91bc59c63baf9fc52c5ead4bcad
Andre Aila Owano <129947595+irakusaReo@users.noreply.github.com>
2025-06-30 23:01:05 +0300

    Fix Google Fonts AbortError in Next.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  optimizeFonts: false,
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
